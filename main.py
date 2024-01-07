24639de4ec422e82ddf1fcc339e6821beaaeead8
Dima Birenbaum <dvlasenko86@gmail.com>
2024-01-07 22:31:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from ultralytics import YOLO
+import torch
+
+# Set to your desired GPU number
+torch.cuda.set_device(0) 
 
 if __name__ == '__main__':
     # Load a model
     model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
     # Train the model
-    results = model.train(data='VOC.yaml', epochs=3, imgsz=640, device=0)
+    results = model.train(data='VOC.yaml', epochs=3, imgsz=640, device='gpu')
 
     metrics = model.val()  # evaluate model performance on the validation set
     results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
